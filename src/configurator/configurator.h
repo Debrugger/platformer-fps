8d6d0d8887dd987c2bd4626988f0dc26433380b0
Thomas <tom@boulderblock.eu>
2017-02-25 15:40:33 +0100

    added src

---
+++ src/configurator/configurator.h
@@ -0,0 +1,37 @@
+#ifndef __CONFIGURATOR_H
+#define __CONFIGURATOR_H
+#include <QApplication>
+#include <QMainWindow>
+#include <QDebug>
+
+#include <QSignalMapper>
+
+#include <QLabel>
+#include <QSettings>
+#include <QPushButton>
+#include <QString>
+#include <QLineEdit>
+#include <QFileDialog>
+
+#include "../../build/configurator/configurator_ui.h"
+
+class MainWindow : public QMainWindow, public Ui::MainWindow  /*Ui:: has to be same as window/widget name in qtcreator .ui file*/
+{
+	Q_OBJECT;
+public:
+	MainWindow();
+	virtual ~MainWindow();
+
+ 	void ReadSettings();
+
+	QPixmap char_pixmap;
+public slots:
+	void QuitClicked();
+	void ButtonClicked(int);
+	void SaveSettings();
+	
+};
+
+
+#endif // __CONFIGURATOR_H
+
